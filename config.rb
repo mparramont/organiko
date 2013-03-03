ca98479fa481ed1d1528e58b017a73fe79cbd36b
Miguel Parramón <mparramont@gmail.com>
2013-03-03 17:41:58 +0100

    Join all css into one, minify html

--- config.rb
+++ config.rb
@@ -59,8 +59,8 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
+  activate :minify_html
 
   # Minify Javascript on build
   # activate :minify_javascript
@@ -69,7 +69,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
